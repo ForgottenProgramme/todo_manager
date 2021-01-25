c4f6ffd1d27e2c3858556bc7adc776da13670d35
ForgottenProgramme <forgotten.programme@gmail.com>
2021-01-26 03:24:10 +0530

    scoping

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,10 @@ class Todo < ActiveRecord::Base
 
   belongs_to :user
 
+  def self.of_user(user)
+    all.where(user_id: user.id)
+  end
+
   def self.overdue
     all.where("due_date < :date", { date: Date.today })
   end
